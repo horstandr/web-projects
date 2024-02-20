19ca45082f500480e2e521c6e99ba5227a3a78d4
horstandr <andras.schiphorst@icloud.com>
2024-02-20 11:20:02 +0100

    Updated gemiddelde berekenen, it works now! :)

--- portfolio/portfolio-websites/Gemiddelde berekenen/script.js
+++ portfolio/portfolio-websites/Gemiddelde berekenen/script.js
@@ -1,6 +1,10 @@
-var n1 = document.querySelector('#id1').innerHTML;
-var n2 = document.querySelector('#id2').innerHTML;
 
 function Calculate() {
-    console.log(n1);
+    var n1 = document.querySelector('#id1').value;
+    var n2 = document.querySelector('#id2').value;
+    n1 = parseInt(n1);
+    n2 = parseInt(n2);
+    var result = n1 + n2;
+    result = result / 2;
+    document.querySelector("#result").innerHTML = result;
 }
