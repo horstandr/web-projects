9e0f16175836256a1164c8363de4aa1285aa527f
horstandr <andras.schiphorst@icloud.com>
2024-02-21 22:21:52 +0100

    V 0.1.9 - HorstAndr Portfolio - 21 feb. 2024
    🚨NEW WEBSITE ALERT!🚨
    There is a new website, a countdown timer! Input the time and date that you are looking forward to and watch as the counter goes down.
    
    New features:
    1. A countown timer!
    
    Other changes:
    2. Changed horstandr to HorstAndr.

---
+++ portfolio/portfolio-websites/countdown-timer/script.js
@@ -0,0 +1,50 @@
+function startCountdown() {
+    var future = true;
+    var bodyForm = document.getElementById('body-form');
+    var now = new Date();
+    var dateTimeStr = document.getElementById('datetime').value;
+    var display = document.getElementById('countdown');
+
+    display.style.display = 'block';
+    bodyForm.style.display = 'none';
+    console.log(dateTimeStr);
+
+    var dateTime = new Date(dateTimeStr);
+    var dateTimeFinal = dateDifference(dateTime, now);
+    
+
+    var days = Math.floor((dateTimeFinal / 86_400_000));
+    var hours = Math.floor((dateTimeFinal % (1000 * 60 * 60 * 24)) / 3_600_000);
+    var minutes = Math.floor((dateTimeFinal % (1000 * 60 * 60)) / 60_000);
+    var seconds = Math.floor((dateTimeFinal % (1000 * 60)) / 1_000);
+
+    display.innerHTML = days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's ';
+
+    if (days == 0 && hours == 0 && minutes == 0 && seconds == 0) {
+        setTimeout(100);
+        alert('The time is here...');
+        location.reload();
+        return;
+    }
+}
+
+function updateStart() {
+    var now = new Date();
+    var dateTimeStr = document.getElementById('datetime').value;
+    var dateTime = new Date(dateTimeStr);
+    var dateTimeFinal = dateDifference(dateTime, now);
+
+    if (Math.sign(dateTimeFinal) == -1) {
+        alert('The date needs to be in the future!');
+        return;
+    } else if (dateTimeStr == '') {
+        alert('Please insert a date and time!');
+        return;
+    } else {
+        setInterval(startCountdown, 1000);
+    }
+}
+
+function dateDifference(finalDate, now) {
+    return finalDate - now; 
+}
