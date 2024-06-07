3ba95b5d6e2a134ba3d71f926e33f4c02badd8e5
horstandr <andras.schiphorst@icloud.com>
2024-06-07 11:47:37 +0200

    Removed the ugly Arial title on the top of the timer, now the timer is in the CENTER. Made the clock bigger, to compensate for the lost space. Made the website automaticcaly fullscreen, and added an exit button.

--- portfolio/portfolio-websites/digital-clock/script-clock.js
+++ portfolio/portfolio-websites/digital-clock/script-clock.js
@@ -9,7 +9,7 @@ function startTime() {
     hours = checkTime(hours);
     seconds = checkTime(seconds);
 
-    clock.innerHTML = hours + 'i' + minutes + 'i' + seconds;
+    clock.innerHTML = hours + '.' + minutes + '.' + seconds;
     setTimeout(startTime, 1000);
 }
 
@@ -19,3 +19,35 @@ function checkTime(i) {
     }
     return i;
 }
+
+// Creates buttons
+const body = document.body;
+const buttons = `<button onclick="openFullscreen()" id="fs-button">Fullscreen</button><br><button onclick="closeFullscreen()" style="display:none" id="exit-button">Exit</button>`
+body.innerHTML += buttons;
+
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+  document.getElementById('fs-button').style.display = "none";
+  document.getElementById('exit-button').style.display = "block";
+}
+
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+      document.msExitFullscreen();
+    }
+    document.getElementById('fs-button').style.display = "block";
+    document.getElementById('exit-button').style.display = "none";
+  }
